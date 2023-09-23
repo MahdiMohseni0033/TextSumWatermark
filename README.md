6f05359d707e9e436a9e93dd677c0bc6acccc9a5
mmohseni <mmohseni.sdstudio@gmail.com>
2023-09-23 17:36:28 +0330

    update the code

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
 # TextSumWatermark
+# Watermarking for Large Language Models
+
+Welcome to the TextSumWatermark repository, dedicated to watermarking large language models. This project is inspired by the groundbreaking paper [A Watermark for Large Language Models](https://arxiv.org/abs/2301.10226) with custom enhancements guided by Professor Yue Dong. 
+
+
+### Here's an overview of the recent additions and improvements to our codebase:
+
+
+- We have expanded our codebase to accommodate summarization models such as T5 and Flan, in addition to just decoder models.
+- We've introduced evaluation capabilities, allowing you to assess different models performance on datasets like CNN_DailyMail and Xsum seamlessly.
+- We've integrated a Gradio demo with the latest models.
+- To provide a deeper understanding of the watermarking approach, we've added a script to generate statistical result plots.
+
+***
+## Overview
+
+The core idea is to insert a subtle but detectable watermark into the generations of a language model. This is done by biasing the model's next token predictions during generation towards a small "greenlist" of candidate tokens determined by the prefix context. The watermark detector then analyzes text and checks if an abnormally high fraction of tokens fall within these expected greenlists, allowing it to distinguish watermarked text from human-written text.
+
+## Contents
+
+The repository contains the following key files:
+
+- `watermark_processor.py`: Implements the `WatermarkLogitsProcessor` class which inserts the watermark during generation, and the `WatermarkDetector` class for analyzing text.
+
+- `utils.py`: Utility functions for loading models, datasets, and running the interactive demo.
+
+- `normalizers.py`: Text normalizers to defend against textual attacks against the watermark detector. 
+
+- `config.yaml`: Configuration file for model and watermark parameters.
+
+- `main.py`: Main script to run watermark insertion and detection on a dataset.
+
+- `gradio_demo.py`: Script to launch an interactive web demo of watermarking using Gradio.
+
+- `homoglyphs.py`: Helper code for managing homoglyph replacements.
+
+- README: This file.
+
+## Prerequisites and Installation
+
+To run TextSumWatermark effectively, please ensure that you have the following prerequisites in place:
+
+- Python Version: We recommend using Python version 3.9 or higher.
+- You'll also need to install the necessary libraries. You can do this by running the following command:
+```pip install -r requirements.txt```
+
+These libraries include PyTorch and Transformers, which are essential for the operation of this codebase. Once you have the prerequisites set up, you're ready to dive into watermarking large language 
+
+## Usage 
+
+There are two main ways to use the code:
+
+### 1. CLI usage
+
+The `main.py` script allows watermarking and detection on an input dataset specified in the config file:
+
+```python main.py```
+
+
+This will load the model, dataset, and parameters from `config.yaml`, run generation with and without watermarks on the dataset, and perform detection on the outputs, saving results to `results.csv`.
+
+
+### 2. Interactive demo
+
+To launch a live demo:
+
+```python gradio_demo.py```
+
+
+This will launch a Gradio web interface allowing interactive prompt generation and watermark detection with parameters adjustable via the UI.
+
+
+## Configuration
+
+The key parameters controlling the watermarking scheme are:
+
+- `gamma`: Fraction of tokens to assign to the greenlist at each step. Lower values make the watermark stronger. 
+- `delta`: Amount of bias/boost applied to greenlist tokens during generation. Higher values make the watermark stronger.
+
+The detector can be configured with:
+
+- `z_threshold`: z-score cutoff to call a positive watermark detection  
+- `normalizers`: Text normalizations to run during detection like lowercasing.
+
+See the paper for detailed discussion of settings and their effect on generation quality and detection accuracy.
+
+
+## Contact
+
+If you have any questions or comments about this project, please don't hesitate to contact us:
+
+Email: [mahdimohseni0333@gmail.com](mailto:mahdimohseni0333@gmail.com)
--- main.py
+++ main.py
@@ -1,136 +1,123 @@
-from pprint import pprint
-
 import yaml
+import pandas as pd
 from argparse import Namespace
 from tqdm import tqdm
-from utils import *
-import pandas as pd
+import warnings
+from utils import load_model, prepare_dataset, generate, detect
 
-def main2(args):
-    args.normalizers = (args.normalizers.split(",") if args.normalizers else [])
-    print(args)
-
-    model, tokenizer, device = load_model(args)
-    dataset = prepare_dataset(dataset_name='xsum', limit=4)
-    df = pd.DataFrame()
-    for input_text, id in tqdm(zip(dataset['document'], dataset['id'])):
-        args.default_prompt = input_text
-        _, _, decoded_output_without_watermark, decoded_output_with_watermark, _ = generate(input_text,
-                                                                                            args,
-                                                                                            model=model,
-                                                                                            device=device,
-                                                                                            tokenizer=tokenizer)
-        without_watermark_detection_result = detect(decoded_output_without_watermark,
-                                                    args,
-                                                    device=device,
-                                                    tokenizer=tokenizer)[0]
-        with_watermark_detection_result = detect(decoded_output_with_watermark,
-                                                 args,
-                                                 device=device,
-                                                 tokenizer=tokenizer)[0]
-
-        result = {}
-        result['id'] = id
-        result['WO_num_tokens_scored'] = without_watermark_detection_result['num_tokens_scored']
-        result['WO_num_green_tokens'] = without_watermark_detection_result['num_green_tokens']
-        result['WO_green_fraction'] = without_watermark_detection_result['green_fraction']
-        result['WO_z_score'] = without_watermark_detection_result['z_score']
-        result['WO_p_value'] = without_watermark_detection_result['p_value']
-        result['WO_prediction'] = without_watermark_detection_result['prediction']
-
-        result['W_num_tokens_scored'] = with_watermark_detection_result['num_tokens_scored']
-        result['W_num_green_tokens']= with_watermark_detection_result['num_green_tokens']
-        result['W_green_fraction']= with_watermark_detection_result['green_fraction']
-        result['W_z_score']= with_watermark_detection_result['z_score']
-        result['W_p_value']= with_watermark_detection_result['p_value']
-        result['W_prediction']= with_watermark_detection_result['prediction']
-
-        # Convert the result dictionary to a DataFrame
-        df_result = pd.DataFrame([result])
-
-        # Append the result DataFrame to the main DataFrame
-        df = pd.concat([df, df_result], ignore_index=True)
-
-        # Save the DataFrame as a CSV file
-        df.to_csv('result.csv', index=False)
+# Ignore all future warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-import pandas as pd
+def load_config(file_path):
+    """
+    Load configuration settings from a YAML file.
+
+    Args:
+        file_path (str): The path to the YAML configuration file.
+
+    Returns:
+        Namespace: A namespace containing the configuration settings.
+    """
+    with open(file_path, 'r') as f:
+        return yaml.safe_load(f)
+
+
+def process_document(input_text, id, args, model, tokenizer, device):
+    """
+    Process a document and generate results.
+
+    Args:
+        input_text (str): The input text to process.
+        id (int): The ID of the document.
+        args (Namespace): Command-line arguments.
+        model: The model for text generation.
+        tokenizer: The tokenizer for text processing.
+        device: The device for computation.
+
+    Returns:
+        dict: A dictionary containing the processing results.
+    """
+    args.default_prompt = input_text
+    _, _, decoded_output_without_watermark, decoded_output_with_watermark, num_token_without_watermark, num_token_with_watermark, _ = generate(
+        input_text,
+        args,
+        model=model,
+        device=device,
+        tokenizer=tokenizer)
+
+    without_watermark_detection_result = detect(decoded_output_without_watermark,
+                                                args,
+                                                device=device,
+                                                tokenizer=tokenizer)[0]
+    with_watermark_detection_result = detect(decoded_output_with_watermark,
+                                             args,
+                                             device=device,
+                                             tokenizer=tokenizer)[0]
+
+    result = {
+        'id': str(id),
+        'WO_num_tokens_scored': without_watermark_detection_result['num_tokens_scored'],
+        'WO_num_green_tokens': without_watermark_detection_result['num_green_tokens'],
+        'WO_green_fraction': without_watermark_detection_result['green_fraction'],
+        'WO_z_score': without_watermark_detection_result['z_score'],
+        'WO_p_value': without_watermark_detection_result['p_value'],
+        'WO_prediction': without_watermark_detection_result['prediction'],
+        'WO_num_output_token': num_token_without_watermark,
+        'W_num_tokens_scored': with_watermark_detection_result['num_tokens_scored'],
+        'W_num_green_tokens': with_watermark_detection_result['num_green_tokens'],
+        'W_green_fraction': with_watermark_detection_result['green_fraction'],
+        'W_z_score': with_watermark_detection_result['z_score'],
+        'W_p_value': with_watermark_detection_result['p_value'],
+        'W_prediction': with_watermark_detection_result['prediction'],
+        'W_num_output_token': num_token_with_watermark,
+    }
+
+    return result
 
 
 def main(args):
-    args.normalizers = (args.normalizers.split(",") if args.normalizers else [])
+    # Parse the 'normalizers' argument into a list
+    args.normalizers = args.normalizers.split(",") if args.normalizers else []
     print(args)
 
+    # Load the model, tokenizer, and device
     model, tokenizer, device = load_model(args)
-    dataset = prepare_dataset(dataset_name='xsum', limit=8)
 
-    # Read the existing results from 'result.csv' if it exists
+    # Prepare the dataset
+    dataset = prepare_dataset(args)
+
+    # Read existing results from 'result.csv' if it exists, or create an empty DataFrame
     try:
         df_existing = pd.read_csv('result.csv')
     except FileNotFoundError:
-        df_existing = pd.DataFrame()
+        df_existing = pd.DataFrame(columns=['id'])
 
     df = pd.DataFrame()
 
-    if 'id' not in df_existing.columns:
-        df_existing['id'] = None
-
     for input_text, id in tqdm(zip(dataset['document'], dataset['id'])):
         # Check if the ID already exists in the existing DataFrame
-        if id in df_existing['id'].values:
+        if str(id) in df_existing['id'].values:
             continue  # Skip processing this ID
 
-        args.default_prompt = input_text
-        _, _, decoded_output_without_watermark, decoded_output_with_watermark, _ = generate(input_text,
-                                                                                            args,
-                                                                                            model=model,
-                                                                                            device=device,
-                                                                                            tokenizer=tokenizer)
-        without_watermark_detection_result = detect(decoded_output_without_watermark,
-                                                    args,
-                                                    device=device,
-                                                    tokenizer=tokenizer)[0]
-        with_watermark_detection_result = detect(decoded_output_with_watermark,
-                                                 args,
-                                                 device=device,
-                                                 tokenizer=tokenizer)[0]
+        result = process_document(input_text, id, args, model, tokenizer, device)
 
-        result = {}
-        result['id'] = id
-        result['WO_num_tokens_scored'] = without_watermark_detection_result['num_tokens_scored']
-        result['WO_num_green_tokens'] = without_watermark_detection_result['num_green_tokens']
-        result['WO_green_fraction'] = without_watermark_detection_result['green_fraction']
-        result['WO_z_score'] = without_watermark_detection_result['z_score']
-        result['WO_p_value'] = without_watermark_detection_result['p_value']
-        result['WO_prediction'] = without_watermark_detection_result['prediction']
-
-        result['W_num_tokens_scored'] = with_watermark_detection_result['num_tokens_scored']
-        result['W_num_green_tokens'] = with_watermark_detection_result['num_green_tokens']
-        result['W_green_fraction'] = with_watermark_detection_result['green_fraction']
-        result['W_z_score'] = with_watermark_detection_result['z_score']
-        result['W_p_value'] = with_watermark_detection_result['p_value']
-        result['W_prediction'] = with_watermark_detection_result['prediction']
-
-        # Convert the result dictionary to a DataFrame
+        # Append the result dictionary to a DataFrame
         df_result = pd.DataFrame([result])
 
         # Append the result DataFrame to the main DataFrame
         df = pd.concat([df, df_result], ignore_index=True)
 
-        # Combine the existing DataFrame and the new results DataFrame
-        df = pd.concat([df_existing, df], ignore_index=True)
+    # Combine the existing DataFrame and the new results DataFrame
+    df = pd.concat([df_existing, df], ignore_index=True)
 
-        # Save the updated DataFrame as a CSV file
-        df.to_csv('result.csv', index=False)
+    # Save the updated DataFrame as a CSV file
+    df.to_csv(args.csv_path, index=False)
+
 
 if __name__ == "__main__":
-    # Load YAML file
-    with open('config.yaml', 'r') as f:
-        config = yaml.safe_load(f)
-
-    args = Namespace()
-
-    args.__dict__.update(config)
-
+    config_path = 'config.yaml'
+    # Load YAML file to initialize the 'args' Namespace
+    config = load_config(config_path)
+    args = Namespace(**config)
     main(args)
